23dd6a727d9129c255fa892679ec1b187422e7b3
thiago.dasilva <thiago.dasilva@softplan.com.br>
2021-05-29 14:23:32 -0300

    moved breadcrumb logic to a hook

--- ecommerce-app/src/presentation/components/Breadcrumb/Breadcrumb.tsx
+++ ecommerce-app/src/presentation/components/Breadcrumb/Breadcrumb.tsx
@@ -1,32 +1,11 @@
 import React from "react";
-import { useLocation } from 'react-router-dom';
-import { Location } from 'history';
-import BreadcrumbView, { TLocation } from "./BreadcrumbView";
+import useBreadCrumb from '../../hooks/useBreadcrumb';
+import BreadcrumbView from './BreadcrumbView';
 
 type Props =  {}
 const Breadcrumb: React.FC<Props> = () => {
-  const location = useLocation();
-
-  const locationItems: Array<TLocation> = [];
-  addLocationItemToHome(locationItems);
-  addLocationItemToProduct(locationItems, location);
-
+  const locationItems = useBreadCrumb();
   return <BreadcrumbView {...{locationItems}}/>;
 };
 
 export default Breadcrumb;
-
-function addLocationItemToHome(locationItems: Array<TLocation>): void {
-  locationItems.push({name: "home", uri: "/"})
-}
-
-function addLocationItemToProduct(locationItems: Array<TLocation>, location: Location): void {
-  const locations = location.pathname.split("/");
-  if (locations.some((path: string) => path === "produto")) {
-    const [, , id ]  = locations;
-    locationItems.push({
-      name: `produto - ${id}`,
-      uri: location.pathname
-    })
-  }
-}
---
+++ ecommerce-app/src/presentation/hooks/useBreadcrumb.ts
@@ -0,0 +1,32 @@
+import { useLocation } from 'react-router-dom';
+import { Location } from 'history';
+
+export default function useBreadcrumb(): Array<TLocation> {
+  const location = useLocation();
+
+  const locationItems: Array<TLocation> = [];
+  addLocationItemToHome(locationItems);
+  addLocationItemToProduct(locationItems, location);
+
+  return locationItems;
+}
+
+function addLocationItemToHome(locationItems: Array<TLocation>): void {
+  locationItems.push({name: "home", uri: "/"})
+}
+
+function addLocationItemToProduct(locationItems: Array<TLocation>, location: Location): void {
+  const locations = location.pathname.split("/");
+  if (locations.some((path: string) => path === "produto")) {
+    const [, , id ]  = locations;
+    locationItems.push({
+      name: `produto - ${id}`,
+      uri: location.pathname
+    })
+  }
+}
+
+export type TLocation = {
+  name: string
+  uri: string
+}
